ad8f74a80accf1f8ae0a0638cac667becca2b43f
Gonzalo Maulella <gonzavv14@gmail.com>
2023-08-22 11:36:10 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,16 +2,16 @@
 document.getElementById("buttonText").addEventListener("click", function(){
     const textInput = document.getElementById("inputText").value;
         if (localStorage.getItem("array")){
-            alert("si");
+            //alert("si");
             let a = JSON.parse(localStorage.getItem("array"));
             a.push(" "+textInput);
             localStorage.setItem("array", JSON.stringify(a));
         } else {
-            alert("no");
+            //alert("no");
             let array = [];
             array.push(textInput);
             localStorage.setItem("array", JSON.stringify(array));
         }
         document.getElementById("inputText").value = null;
     }
-)
+);
